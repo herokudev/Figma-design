5399c3ae1c4df79042b31dd6315af8bac29c751b
herokudev <ao43085@gmail.com>
2021-08-02 16:22:14 -0600

    Changes for styling and JavaScript
    Changes for styling and JavaScript

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-const menuIcon = document.querySelector('.mobile-menu');
+const menuIcon = document.querySelector('.hamburger-menu');
+const navBar = document.querySelector('.navbar');
+const menuItem1 = document.querySelector('#menu-item1');
+const menuItem2 = document.querySelector('#menu-item2');
+const menuItem3 = document.querySelector('#menu-item3');
+
+menuIcon.addEventListener('click',(event) => {
+    navBar.classList.toggle("change");
+});
