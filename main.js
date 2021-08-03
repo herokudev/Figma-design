51322be39a3547007353c1789446e741717413a1
herokudev <ao43085@gmail.com>
2021-08-03 09:14:52 -0600

    Adding html elements and pop window mobile
    Adding html elements and pop window mobile

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const menuIcon = document.querySelector('.hamburger-menu');
 const navBar = document.querySelector('.navbar');
+const modal = document.querySelector('#simpleModal');
+const modalbtn = document.querySelector('.modalbtn');
+const closeBtn = document.querySelector('.closeBtn');
 const menuItem1 = document.querySelector('#menu-item1');
 const menuItem2 = document.querySelector('#menu-item2');
 const menuItem3 = document.querySelector('#menu-item3');
@@ -18,4 +21,12 @@ menuItem2.addEventListener('click', () => {
 
 menuItem3.addEventListener('click', () => {
   navBar.classList.toggle('change');
+});
+
+modalbtn.addEventListener('click', () => {
+  modal.style.display = 'block';
+});
+
+closeBtn.addEventListener('click', () => {
+  modal.style.display = 'none';
 });
