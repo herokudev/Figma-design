14819ccb7ea4f0375d42404a44f9338096f592a0
herokudev <ao43085@gmail.com>
2021-08-03 14:57:44 -0600

    Changes for desktop popup details
    Changes for desktop popup details

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ const navBar = document.querySelector('.navbar');
 const modal = document.querySelector('#simpleModal');
 const modalbtn = document.querySelector('.modalbtn');
 const closeBtn = document.querySelector('.closeBtn');
+const modalbtn2 = document.querySelector('#desktop-modal-btn');
+const desktopmodal = document.querySelector('#desktopModal');
+const closeBtn2 = document.querySelector('.d-closeBtn');
 const menuItem1 = document.querySelector('#menu-item1');
 const menuItem2 = document.querySelector('#menu-item2');
 const menuItem3 = document.querySelector('#menu-item3');
@@ -29,4 +32,12 @@ modalbtn.addEventListener('click', () => {
 
 closeBtn.addEventListener('click', () => {
   modal.style.display = 'none';
+});
+
+modalbtn2.addEventListener('click', () => {
+  desktopmodal.style.display = 'block';
+});
+
+closeBtn2.addEventListener('click', () => {
+  desktopmodal.style.display = 'none';
 });
