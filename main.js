85b80f05c19ce6910e1a01fe0d7fb6f58a99eeaf
herokudev <ao43085@gmail.com>
2021-08-05 11:21:01 -0600

    Save and load local storage
    Save and load local storage

--- main.js
+++ main.js
@@ -10,11 +10,19 @@ const menuItem1 = document.querySelector('#menu-item1');
 const menuItem2 = document.querySelector('#menu-item2');
 const menuItem3 = document.querySelector('#menu-item3');
 const desktopsubmit = document.querySelector('#form-submit-desktop');
-const emaildesktop = document.querySelector('#email-desktop');
+const name_desktop = document.querySelector('#name-desktop');
+const email_desktop = document.querySelector('#email-desktop');
+const msg_desktop = document.querySelector('#msg-desktop');
 const errormsgdesktop = document.querySelector('#errormsgdesktop');
 const mobileSubmit = document.querySelector('#form-submit-mobile');
-const emailmobile = document.querySelector('#email-mobile');
+const name_mobile = document.querySelector('#name-mobile');
+const email_mobile = document.querySelector('#email-mobile');
+const msg_mobile = document.querySelector('#msg-mobile');
 const errormsgmobile = document.querySelector('#errormsgmobile');
+var d_formInfo = {name:'', email:'', msg:''};
+var m_formInfo = {name:'', email:'', msg:''};
+var desktop_info = "";
+var mobile_info = "";
 
 menuIcon.addEventListener('click', () => {
   navBar.classList.toggle('change');
@@ -49,7 +57,7 @@ closeBtn2.addEventListener('click', () => {
 });
 
 desktopsubmit.addEventListener('click', () => {
-  const str = emaildesktop.value;
+  const str = email_desktop.value;
   const myEmail = str.toLowerCase();
   if (str !== myEmail) {
     errormsgdesktop.style.display = 'block';
@@ -59,11 +67,41 @@ desktopsubmit.addEventListener('click', () => {
 });
 
 mobileSubmit.addEventListener('click', () => {
-  const str = emailmobile.value;
+  const str = email_mobile.value;
   const myEmail = str.toLowerCase();
   if (str !== myEmail) {
     errormsgmobile.style.display = 'block';
   } else {
     errormsgmobile.style.display = 'none';
+  }
+});
+
+name_desktop.addEventListener('input', () => {
+  d_formInfo.name = name_desktop.value;
+  desktop_info = JSON.stringify(d_formInfo);
+  console.log(desktop_info);
+  localStorage.setItem('desktop_data', desktop_info);
+});
+
+email_desktop.addEventListener('input', () => {
+  d_formInfo.email = email_desktop.value;
+  desktop_info = JSON.stringify(d_formInfo);
+  console.log(desktop_info);
+  localStorage.setItem('desktop_data', desktop_info);
+});
+
+msg_desktop.addEventListener('input', () => {
+  d_formInfo.msg = msg_desktop.value;
+  desktop_info = JSON.stringify(d_formInfo);
+  console.log(desktop_info);
+  localStorage.setItem('desktop_data', desktop_info);
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const d_savedData = JSON.parse(localStorage.getItem('desktop_data'));
+  if (d_savedData !== null) {
+    name_desktop.value = d_savedData.name;
+    email_desktop.value = d_savedData.email;
+    msg_desktop.value = d_savedData.msg;
   }
 });
